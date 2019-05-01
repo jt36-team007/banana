801a88e7f93fdeeb9ed952e6069ddfe40dbf68e6
elecland <elecland@126.com>
2019-05-01 11:29:31 +0800

    修改了static页面

--- src/main/webapp/WEB-INF/static/js/index.js
+++ src/main/webapp/WEB-INF/static/js/index.js
@@ -13,3 +13,24 @@ $(function(){
 function inMfg(rfqno) {
     window.location.href=getRootPath()+"/inMfg/"+rfqno;
 }
+
+
+var w = 1;
+var aLRB = document.getElementsByClassName("bannerArrow");
+
+function changeBG(der) {
+    if(der==-1){
+        --w<0?w=3:"";
+    }
+    else{
+        ++w==4?w=1:"";
+    }
+    document.getElementsByClassName("bannerMiddle")[0].className="bannerMiddle bannerImage"+w;
+}
+aLRB[0].onclick = function () {
+    changeBG(-1);
+}
+aLRB[1].onclick = function () {
+    changeBG(1);
+}
+setInterval("changeBG(1)",3000);

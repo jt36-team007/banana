84fdaf20a901afd7e8c0ea7cb2c2f84d521938e7
OverYearLikeWater <504317962@qq.com>
2019-05-03 17:15:08 +0800

    新增mfg rfq 修改页面addMfg  index

---
+++ src/main/java/com/uiqun/controller/MfgController.java
@@ -0,0 +1,51 @@
+package com.uiqun.controller;
+
+import com.uiqun.model.Mfg;
+import com.uiqun.model.User;
+import com.uiqun.service.PntypeService;
+import com.uiqun.utils.Encrypt_Dncrypt;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class MfgController {
+    @Resource
+    private PntypeService pntypeService;
+    //跳转网页
+    @RequestMapping("/insertmfg")
+    public String mfg(){
+        return "addMfg";
+    }
+    //新增品牌
+    @RequestMapping("/addmfg")
+    public String addMfg(Model model, Mfg mfg, MultipartFile upload, HttpSession session){
+        if(upload!=null){
+            StringBuilder stringBuilder = new StringBuilder(upload.getName());
+            User u = (User)session.getAttribute("user");
+            stringBuilder.append(u.getUid());
+            String MD5_upload = Encrypt_Dncrypt.getMD5(stringBuilder.toString().getBytes());
+            String ofn = upload.getOriginalFilename();
+            String filename=MD5_upload+ofn.substring(ofn.lastIndexOf("."),ofn.length());
+            mfg.setMlogo(filename);
+            try {
+                String upfilelogin = session.getServletContext().getRealPath("upfilelogin");
+                    upload.transferTo(new File(upfilelogin,filename)
+                );
+            //添加到数据库的没写
+
+                model.addAttribute("AlertMessage","添加品牌成功");
+            } catch (IOException e) {
+                model.addAttribute("AlertMessage","添加品牌失败");
+                e.printStackTrace();
+            }
+        }
+        return "forward:/insertmfg";
+    }
+}
--- src/main/java/com/uiqun/controller/RfqController.java
+++ src/main/java/com/uiqun/controller/RfqController.java
@@ -50,4 +50,5 @@ public class RfqController {
         }
         return null;
     }
+
 }
